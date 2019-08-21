40d2eff3719b2ca4e7be8619af497e5f34c7450f
Pamela <pamablang@gmail.com>
2019-08-22 09:38:04 +1000

    Create option to make offer

--- routes/offers.rb
+++ routes/offers.rb
@@ -1,2 +1,22 @@
-get '/offers' do
+# params need to be sent from the items page
+get '/offers/:item_id' do
+
+  @proposed_items = Item.where(user_id: current_user.id)
+  @reviewer_item = Item.find(params[:item_id])
+  # binding.pry
+  @photos = Photo.where(item_id: params[:item_id])
+  erb :offers_new
+end
+
+post '/offers/new' do
+  o = Offer.new
+  o.proposer_user_id = current_user.id
+  o.proposer_item_id = Item.find(params[:item_id])
+  o.proposer_item_qty = Item.find(params[:qty])
+  o.reviewer_user_id = Item.find(params[:reviewer_item_id]).user_id
+  o.reviewer_item_id = Item.find(params[:reviewer_item_id])
+  o.status_id = 1
+  o.save
+  redirect "/offers/#{params[:item_id]}"
+  # redirect to a transactions page
 end
