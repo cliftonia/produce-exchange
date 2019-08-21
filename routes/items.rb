7b0b6820d905b63afc97155393e211dbbfb3793a
Clifton <clifton.baggerman@me.com>
2019-08-21 17:51:32 +1000

    Create new item page, edit item page, created Image Uploader class which as well as put in my aws configuration in my items.rb

--- routes/items.rb
+++ routes/items.rb
@@ -1,2 +1,43 @@
-get '/items' do
+get '/items/new' do
+
+  erb :item_new
+end
+
+post '/items' do
+  item = Item.new
+  item.title = params[:title]
+  item.description = params[:description]
+  item.quantity = params[:quantity]
+  item.unit = params[:unit]
+  item.user_id = current_user.id
+  item.save
+  
+  photo = Photo.new
+  photo.image_link = params[:image_link]
+  photo.item_id = item.id
+  photo.save
+
+  redirect '/'
+end
+
+get '/items/:id/edit' do 
+  @item = Item.find(params[:id])
+  @photo = Photo.find_by(item_id: params[:id])
+  erb :item_edit
+end
+
+put '/items/:id' do
+
+  item = Item.find(params[:id])
+  item.title = params[:title]
+  item.description = params[:description]
+  item.quantity = params[:quantity]
+  item.unit = params[:unit]
+  item.save
+
+  photo = Photo.find_by(item_id: params[:id])
+  photo.image_link = params[:image_link]
+  photo.save
+
+  redirect '/'
 end
