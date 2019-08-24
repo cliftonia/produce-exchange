1d54d913c4b4da7b546645c05176785543e0c5d6
Ashley Martin <amartin3112@gmail.com>
2019-08-24 15:35:21 +1000

    Add validation to enforce new users to provide a suburb

--- models/user.rb
+++ models/user.rb
@@ -6,5 +6,6 @@ class User < ActiveRecord::Base
   # validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: 'Invalid email format' }, uniqueness: true, presence: true
   # validates :username, presence: true, uniqueness: true
   # validates :password, length: { minimum: 8 }
+  # validates :suburb, presence: true
   # validates :postcode, length: { is: 4 }
 end
